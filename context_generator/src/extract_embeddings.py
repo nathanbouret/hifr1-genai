8e4669bfa6394e1d59a4257d4e214693118fc625
BAY AHMED Hadj Ahmed <hadj-ahmed.bay-ahmed@altran.com>
2023-07-05 10:59:10 +0200

    [refactor] added files in context_generator module

--- context_generator/src/extract_embeddings.py
+++ context_generator/src/extract_embeddings.py
@@ -1,13 +1,57 @@
+import time
+from typing import List
+
 from langchain.vectorstores import FAISS
 from langchain.vectorstores import Chroma
+from langchain.embeddings import VertexAIEmbeddings
+from pydantic import BaseModel
 
 
+class CustomVertexAIEmbeddings(VertexAIEmbeddings, BaseModel):
+    requests_per_minute: int
+    num_instances_per_batch: int
 
-def extract_embeddings_from_documents(documents, embedding_model, vector_store_name):
-    print(vector_store_name)
+    # Overriding embed_documents method
+    def embed_documents(self, texts: List[str]):
+        limiter = rate_limit(self.requests_per_minute)
+        results = []
+        docs = list(texts)
+
+        while docs:
+            # Working in batches because the API accepts maximum 5
+            # documents per request to get embeddings
+            head, docs = (
+                docs[: self.num_instances_per_batch],
+                docs[self.num_instances_per_batch :],
+            )
+            chunk = self.client.get_embeddings(head)
+            results.extend(chunk)
+            next(limiter)
+
+        return [r.values for r in results]
+
+
+# Utility functions for Embeddings API with rate limiting
+def rate_limit(max_per_minute):
+    period = 60 / max_per_minute
+    print("Waiting")
+    while True:
+        before = time.time()
+        yield
+        after = time.time()
+        elapsed = after - before
+        sleep_time = max(0, period - elapsed)
+        if sleep_time > 0:
+            print(".", end="")
+            time.sleep(sleep_time)
+
+
+def extract_embeddings_from_documents(documents, embedding_model, vector_store_method='FAISS'):
+    print(vector_store_method)
     # Store docs in local vectorstore as index
-    if vector_store_name == 'FAISS':
+    if vector_store_method == 'FAISS':
         vector_store = FAISS.from_documents(documents, embedding_model)
-    elif vector_store_name == 'chroma':
-        vector_store = Chroma.from_documents(documents, embeddings)
+    elif vector_store_method == 'chroma':
+        vector_store = Chroma.from_documents(documents, embedding_model)
     return vector_store
+
